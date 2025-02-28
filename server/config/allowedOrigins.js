d88e25733e8e4e8a1960de8299d11710bf5f2982
Vedansh Sharma <svedansh0302@gmail.com>
2025-03-01 00:41:24 +0530

    atlass

--- server/config/allowedOrigins.js
+++ server/config/allowedOrigins.js
@@ -1,11 +1,6 @@
 const allowedOrigins = [
-  "https://recipe-client-six.vercel.app/",
-  "https://recipe-eta-brown-46.vercel.app/",
+  "https://recipe-client-six.vercel.app",
+  "https://recipe-eta-brown-46.vercel.app",
 ];
-
-// const allowedOrigins = [
-//   "http://localhost:5173",
-//   "http://localhost:5000",
-// ];
 
 module.exports = allowedOrigins;
--- server/config/corsOptions.js
+++ server/config/corsOptions.js
@@ -2,13 +2,16 @@ const allowedOrigins = require("./allowedOrigins");
 
 const corsOptions = {
   origin: (origin, callback) => {
-    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+    if (!origin || allowedOrigins.includes(origin)) {
       callback(null, true);
     } else {
       callback(new Error("Not allowed by CORS"));
     }
   },
+  credentials: true, // Allow credentials if needed
   optionsSuccessStatus: 200,
+  allowedHeaders: ["Content-Type", "Authorization"], // Explicitly allow headers
+  methods: ["GET", "POST", "PUT", "DELETE"], // Allowed HTTP methods
 };
 
 module.exports = corsOptions;
