7eaf6f3a01dbd61ae60a29e1082e27f752ed0fc0
Vedansh Sharma <svedansh0302@gmail.com>
2025-03-01 00:24:13 +0530

    changeorigin

--- server/config/allowedOrigins.js
+++ server/config/allowedOrigins.js
@@ -1,11 +1,11 @@
-// const allowedOrigins = [
-//   "https://CookLab.vercel.app",
-//   "https://CookLab-backend.onrender.com",
-// ];
-
 const allowedOrigins = [
-  "http://localhost:5173",
-  "http://localhost:5000",
+  "https://recipe-client-six.vercel.app/",
+  "https://recipe-eta-brown-46.vercel.app/",
 ];
+
+// const allowedOrigins = [
+//   "http://localhost:5173",
+//   "http://localhost:5000",
+// ];
 
 module.exports = allowedOrigins;
