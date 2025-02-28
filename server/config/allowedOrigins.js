350e4f0059d3817f22711a1a509974febbd5dbc7
Vedansh Sharma <svedansh0302@gmail.com>
2025-03-01 01:02:49 +0530

    nw

--- server/config/allowedOrigins.js
+++ server/config/allowedOrigins.js
@@ -1,6 +1,11 @@
+// const allowedOrigins = [
+//   "https://CookLab.vercel.app",
+//   "https://recipe-eta-brown-46.vercel.app/",
+// ];
+
 const allowedOrigins = [
-  "https://recipe-client-six.vercel.app",
-  "https://recipe-eta-brown-46.vercel.app",
+  "http://localhost:5173",
+  "http://localhost:5000",
 ];
 
 module.exports = allowedOrigins;
--- server/config/corsOptions.js
+++ server/config/corsOptions.js
@@ -2,16 +2,13 @@ const allowedOrigins = require("./allowedOrigins");
 
 const corsOptions = {
   origin: (origin, callback) => {
-    if (!origin || allowedOrigins.includes(origin)) {
+    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
       callback(null, true);
     } else {
       callback(new Error("Not allowed by CORS"));
     }
   },
-  credentials: true, // Allow credentials if needed
   optionsSuccessStatus: 200,
-  allowedHeaders: ["Content-Type", "Authorization"], // Explicitly allow headers
-  methods: ["GET", "POST", "PUT", "DELETE"], // Allowed HTTP methods
 };
 
 module.exports = corsOptions;
