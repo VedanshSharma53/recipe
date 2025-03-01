64869225617803ef01c9b59de0ec90b129407a47
Vedansh Sharma <svedansh0302@gmail.com>
2025-03-01 11:07:27 +0530

    corsn

--- server/config/corsOptions.js
+++ server/config/corsOptions.js
@@ -1,7 +1,7 @@
 const allowedOrigins = require("./allowedOrigins");
 
 const corsOptions = {
-  origin:["https://recipe-backend-two.vercel.app"],
+  origin:["https://recipe-backend-two.vercel.app","https://recipe-livid-omega.vercel.app"],
   credentials: true,  // Allow cookies/session tokens
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   optionsSuccessStatus: 200,
