b39deaff7045260e2bc6b2241ba9a9c2ab7bb9d7
Vedansh Sharma <svedansh0302@gmail.com>
2025-03-01 07:13:54 +0530

    oddf

--- server/config/allowedOrigins.js
+++ server/config/allowedOrigins.js
@@ -1,11 +1,11 @@
-const allowedOrigins = [
-  "https://recipe-livid-omega.vercel.app",
-  "https://recipe-backend-two.vercel.app",
-];
-
 // const allowedOrigins = [
-//   "http://localhost:5173",
-//   "http://localhost:5000",
+//   "https://recipe-livid-omega.vercel.app",
+//   "https://recipe-backend-two.vercel.app",
 // ];
+
+const allowedOrigins = [
+  "http://localhost:5173",
+  "http://localhost:5000",
+];
 
 module.exports = allowedOrigins;
